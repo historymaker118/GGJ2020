0792902c4e1176da699fb93ef34e575c3ee7ea2e
William Terry <Tezza48is@aol.com>
2020-01-31 22:11:09 +0000

    Starting work on paddle controls.
    * Updated project to 2018 LTS.
    + Added PlayerView.
    + Added GameController.

---
+++ Unity Project/Assets/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ControllerType
+{
+    KeyboardLeft,
+    KeyboardRight,
+    Controller
+}
+
+[Serializable]
+public struct Player
+{
+    public ControllerType controllerType;
+    public PlayerView view;
+}
+
+public class GameController : MonoBehaviour
+{
+    public Player playerL;
+    public Player playerR;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlayer(playerL);
+        UpdatePlayer(playerR);
+    }
+
+    private void UpdatePlayer(Player player)
+    {
+        switch (player.controllerType)
+        {
+            case ControllerType.KeyboardLeft:
+                if (Input.GetKey(KeyCode.W))
+                {
+                    player.view.MoveUp();
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    player.view.MoveDown();
+                }
+                break;
+            case ControllerType.KeyboardRight:
+                if (Input.GetKey(KeyCode.I))
+                {
+                    player.view.MoveUp();
+                }
+                if (Input.GetKey(KeyCode.K))
+                {
+                    player.view.MoveDown();
+                }
+                break;
+            case ControllerType.Controller:
+                var axis = Input.GetAxisRaw("Vertical");
+                if (axis > 0.0f)
+                {
+                    player.view.MoveUp();
+                }
+                if (axis < 0.0f)
+                {
+                    player.view.MoveDown();
+                }
+                break;
+        }
+    }
+}
---
+++ Unity Project/Assets/PlayerView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+
+    private readonly float speed = 2.0f;
+
+    private void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveUp()
+    {
+        _rigid.velocity += Vector2.up * speed;
+    }
+
+    public void MoveDown()
+    {
+        _rigid.velocity += Vector2.up * -speed;
+    }
+}
