41f6d75250e274955193ddf78c2774816a522e33
historymaker118 <>
2020-02-01 12:02:08 +0000

    New ball prefab

--- Unity Project/Assets/BallView.cs
+++ Unity Project/Assets/BallView.cs
@@ -3,7 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Currently used basically as a tag.
+// Not anymore ;)
 public class BallView : MonoBehaviour
 {
+    public GameObject solidBall;
 
+
+    public void Start()
+    {
+        solidBall.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<PaddleView>() != null)
+        {
+            StartCoroutine(TimeOutBall());
+        }
+    }
+
+    private IEnumerator TimeOutBall()
+    {
+        solidBall.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        solidBall.SetActive(false);
+    }
 }
