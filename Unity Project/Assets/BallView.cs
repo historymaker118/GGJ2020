4f9739d0052304092be855da042e7b5538df4ace
William Terry <Tezza48is@aol.com>
2020-02-01 18:35:58 +0000

    Removed particle effect stuff from ball.

--- Unity Project/Assets/BallView.cs
+++ Unity Project/Assets/BallView.cs
@@ -6,10 +6,6 @@ using UnityEngine;
 // Not anymore ;)
 public class BallView : MonoBehaviour
 {
-    public GameObject solidBall;
-
-    private Coroutine timeOut;
-
     private Rigidbody2D _rigid;
 
     public Rigidbody2D Rigid { get => _rigid; }
@@ -21,7 +17,7 @@ public class BallView : MonoBehaviour
 
     public void Start()
     {
-        solidBall.SetActive(false);
+
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -29,27 +25,11 @@ public class BallView : MonoBehaviour
         var paddle = col.gameObject.GetComponent<PaddleView>();
         if (paddle != null)
         {
-            if (timeOut != null)
-            {
-                StopCoroutine(timeOut);
-            }
-
-            timeOut = StartCoroutine(TimeOutBall());
-
             GameController.instance.OnPaddleBallCollision(paddle, _rigid);
         }
         else if (col.gameObject.GetComponent<BallResetOnHit>() != null)
         {
             GameController.instance.ResetBall();
         }
-    }
-
-    private IEnumerator TimeOutBall()
-    {
-        solidBall.SetActive(true);
-
-        yield return new WaitForSeconds(0.15f);
-
-        solidBall.SetActive(false);
     }
 }
