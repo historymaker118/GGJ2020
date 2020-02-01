7a4759f16dcd92f8c7a6a933c1e353fa55212ff7
William Terry <Tezza48is@aol.com>
2020-02-01 12:22:34 +0000

    Distance scaling.
    * Paddle distance and camera scale is modifiable using `paddleDistance`.
    * Fixed IEnumerator in BallView.

--- Unity Project/Assets/BallView.cs
+++ Unity Project/Assets/BallView.cs
@@ -8,24 +8,33 @@ public class BallView : MonoBehaviour
 {
     public GameObject solidBall;
 
+    private Coroutine timeOut;
+
 
     public void Start()
     {
         solidBall.SetActive(false);
     }
 
-    private void OnCollisionEnter(Collision col)
+    private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.GetComponent<PaddleView>() != null)
         {
-            StartCoroutine(TimeOutBall());
+            if (timeOut != null)
+            {
+                StopCoroutine(timeOut);
+            }
+
+            timeOut = StartCoroutine(TimeOutBall());
         }
     }
 
     private IEnumerator TimeOutBall()
     {
         solidBall.SetActive(true);
+
         yield return new WaitForSeconds(0.15f);
+
         solidBall.SetActive(false);
     }
 }
--- Unity Project/Assets/GameController.cs
+++ Unity Project/Assets/GameController.cs
@@ -18,14 +18,38 @@ public struct Player
     public PaddleView view;
 }
 
+public struct Bounds
+{
+    float min;
+    float max;
+}
+
+public struct Environment
+{
+    BoxCollider2D topWall;
+    BoxCollider2D bottomWall;
+    BoxCollider2D leftWall;
+    BoxCollider2D rightWall;
+}
+
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
+
+    public float MinPaddleDistance = 6.0f;
+    public float MaxPaddleDistance = 40.0f;
+
+    public float MinCameraScale = 5.0f;
+    public float MaxCameraScale = 25.0f;
 
     public Player playerL;
     public Player playerR;
 
     public Rigidbody2D ball;
+
+    public Environment env;
+
+    new public Camera camera;
 
     public float paddleSpeed = 3.0f;
     public float paddleDrag = 20.0f;
@@ -33,6 +57,9 @@ public class GameController : MonoBehaviour
     public float ballSpeed = 12.0f;
 
     public Vector2 ballDirScale = new Vector2(1.0f, 0.2f);
+
+    [Range(0.0f, 1.0f)]
+    public float paddleDistance = 0.05f;
 
     private void Awake()
     {
@@ -62,13 +89,20 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        FixedUpdatePlayer(playerL);
-        FixedUpdatePlayer(playerR);
+        FixedUpdatePlayer(playerL, false);
+        FixedUpdatePlayer(playerR, true);
+
+        FixedUpdateEnvironment();
     }
 
-    private void FixedUpdatePlayer(Player player)
+    private void FixedUpdatePlayer(Player player, bool isRight)
     {
         var paddleView = player.view;
+
+        var pos = paddleView.transform.position;
+        pos.x = Mathf.Lerp(MinPaddleDistance, MaxPaddleDistance, paddleDistance) * ((isRight) ? 1.0f : -1.0f);
+
+        paddleView.transform.position = pos;
 
         float moveScale = 0.0f;
 
@@ -90,6 +124,12 @@ public class GameController : MonoBehaviour
         }
 
         paddleView.Move(paddleSpeed * moveScale);
+    }
+
+    private void FixedUpdateEnvironment()
+    {
+        camera.orthographicSize = Mathf.Lerp(MinCameraScale, MaxCameraScale, paddleDistance);
+        // TODO: Update Game Walls.
     }
 
     public void OnPaddleBallCollision(PaddleView view, Rigidbody2D ball)
